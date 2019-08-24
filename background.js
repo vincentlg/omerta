ee77d670a7e8aa7901a60a4988f2464df2d02ffe
vincentlg <legallic.vincent@gmail.com>
2019-08-24 22:34:07 +0200

    the tired commit

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ chrome.runtime.onInstalled.addListener(function() {
    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
+          pageUrl: {urlContains: 'com'},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
--- options.js
+++ options.js
@@ -1,26 +1,8 @@
 
 'use strict';
 
- let page = document.getElementById('buttonDiv');
-  const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-  function constructOptions(kButtonColors) {
-    for (let item of kButtonColors) {
-      let button = document.createElement('button');
-      button.style.backgroundColor = item;
-      button.addEventListener('click', function() {
-        chrome.storage.sync.set({color: item}, function() {
-          console.log('color is ' + item);
-        })
-      });
-      //page.appendChild(button);
-    }
-  }
-  
-  constructOptions(kButtonColors);
 
-  document.getElementById("importAccountButton").addEventListener("click", function(){
-    
-    console.log('ok');
+document.getElementById("importAccountButton").addEventListener("click", function(){
 
     const senderPrivateKey = document.getElementById("skInput").value;
     const senderPublicKey = EthCrypto.publicKeyByPrivateKey(senderPrivateKey);
@@ -32,9 +14,89 @@
       publicKey: senderPublicKey
     }
 
-    document.getElementById("result").innerHTML = account
-    chrome.storage.sync.set({account: account}, function() {
-      console.log('account is ' + account);
-    })
+    document.getElementById("skResult").innerHTML = 'Your account is loaded:'+account.address
+    chrome.storage.sync.set({account: account}, function() {})
 
+});
+
+document.getElementById("importPubKeyButton").addEventListener("click", function(){
+
+    const pubKey = document.getElementById("pkInput").value;
+    sendTxToPubKey(pubKey)
+});
+
+async function sendTxToPubKey(recipientPubKey) {
+
+  chrome.storage.sync.get('account', function(data) {
+    
+    let sender = data.account;
+
+    const recipientAddress = EthCrypto.publicKey.toAddress(recipientPubKey);
+
+    const recipient = {
+      address: recipientAddress,
+      publicKey: recipientPubKey
+    }
+
+    const secretMessage = 'iloveyou';
+
+    sendWithSecret(sender, recipient, secretMessage)
+    
+  });
+
+}
+
+async function sendWithSecret(sender, recipient, secretMessage) {
+  
+  const node = 'https://ropsten.infura.io/'
+  const web3 = new Web3( new Web3.providers.HttpProvider(node))
+
+    const encrypted = await EthCrypto.encryptWithPublicKey(
+        recipient.publicKey, secretMessage
+    );
+    const encryptedString = EthCrypto.cipher.stringify(encrypted);
+
+    let data = Web3.utils.utf8ToHex('omerta:'+document.getElementById("socialID").value+':'+encryptedString)
+
+    let nonce = await web3.eth.getTransactionCount(sender.address)
+
+    let details = {
+      "to": recipient.address,
+      "value": 0,
+      "gas": 70000,
+      "gasPrice": 2,
+      "nonce": nonce,
+      "data": data,
+    }
+
+    let _EthereumTx = EthereumTx.Transaction
+
+    const transaction = new _EthereumTx(details, { chain: 'ropsten', hardfork: 'petersburg' },)
+
+    Buffer = Buffer.Buffer
+    const privateKey = Buffer.from(
+      sender.privateKey,
+      'hex',
+    )
+    transaction.sign(privateKey)
+
+    const serializedTransaction = transaction.serialize()
+
+
+    web3.eth.sendSignedTransaction('0x' + serializedTransaction.toString('hex'))
+    .on('transactionHash', async function(hash){
+      console.log(hash);
+      
+      document.getElementById("pubkeyResult").innerHTML = 'Your TX is on the road :'+hash
+    })
+    .on('error', function(err){
+      console.log(err);
+    })
+}
+
+chrome.storage.sync.get('account', function(data) {
+  if(data.account)
+  {
+    document.getElementById("actualKey").innerHTML = 'Your actual address is:'+data.account.address
+  }
 });
