580a97f530d78f1a693067c18cb1f707bb0eab27
tclairet <tcl@blockchain-studio.com>
2019-08-25 02:20:07 +0200

    message ok

---
+++ app.js
@@ -0,0 +1,27 @@
+console.log("app");
+
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    console.log("received message");
+    console.log(msg)
+    if (msg.text === 'secrets') {
+        console.log(msg.secrets);
+        localStorage.setItem("secrets", JSON.stringify(msg.secrets))
+    }
+});
+
+var s5 = document.createElement('script');
+s5.src = chrome.extension.getURL('cryptojs.js');
+(document.head || document.documentElement).appendChild(s5);
+s5.onload = function() {
+  s5.parentNode.removeChild(s5);
+};
+
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('inject.js');
+(document.head || document.documentElement).appendChild(s);
+s.onload = function() {
+  s.parentNode.removeChild(s);
+};
+var retrievedObject = localStorage.getItem('secrets');
+console.log('retrievedObject: ', JSON.parse(retrievedObject));
+console.log(JSON.parse(retrievedObject));
--- popup.js
+++ popup.js
@@ -1,10 +1,18 @@
 'use strict';
 
+let prefix = "omerta"
+var web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/18b3c51dbbdf42d1be2db61e945f88a9"));
 
-let changeColor = document.getElementById('changeColor');
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+function nullHandler(truc) {
+  console.log("good");
+}
+
+chrome.tabs.query({active: true, currentWindow: true}, async function(tabs) {
+  chrome.storage.sync.get('account', async function(data) {
+    let secrets = await getSecrets(data)
+    console.log("send message");
+    chrome.tabs.sendMessage(tabs[0].id, {text: 'secrets', secrets: secrets}, nullHandler);
+  });
 });
 
 chrome.storage.sync.get('account', function(data) {
@@ -18,16 +26,46 @@ showOptionBt.onclick = function(element) {
 	chrome.tabs.create({ 'url': 'chrome://extensions/?options=' + chrome.runtime.id });
 };
 
+async function getSecrets(data) {
+  try {
+    const response = await axios.get('https://api-ropsten.etherscan.io/api?module=account&action=txlist&address=' + data.account.address + '&startblock=0&endblock=99999999&sort=asc&apikey=a');
+    console.log(response.data.result);
+    let transactions = response.data.result
+    let _secrets = new Object();
+    for (var i = 0; i < transactions.length; i++) {
+      if (transactions[i].to.toLowerCase() == data.account.address.toLowerCase()) {
+        console.log("to " + transactions[i].to);
+        var str = web3.utils.hexToUtf8(transactions[i].input);
+        console.log("data " + web3.utils.hexToUtf8(transactions[i].input));
 
-  changeColor.onclick = function(element) {
-  	console.log('---hello---');
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
+        var res = str.split(":")
+        if (res[0] == prefix) {
+          console.log("omerta tx found");
 
-    
-  };
-  
+          let secret = await decodeSecret(res[2], data.account.privateKey)
+          if (secret != "") {
+            _secrets[res[1]] = secret;
+          }
+        }
+      }
+    }
+    return _secrets
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function decodeSecret(str, privateKey) {
+  console.log(privateKey);
+  try {
+    let encodedSecret = EthCrypto.cipher.parse(str)
+    const decrypted = await EthCrypto.decryptWithPrivateKey(
+      privateKey,
+      encodedSecret
+    );
+    return decrypted
+  } catch (e) {
+    console.log(e);
+    return ""
+  }
+}
